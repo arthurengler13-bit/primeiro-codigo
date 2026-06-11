4749f9d3f6b257dc898c1e73e3da5f2c23f8d888
Arthur Engler <arthur_engler150@estudante.sesisenai.org.br>
2026-06-10 21:51:14 -0300

    par e imapar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Escreva a mensagem "imforme um numero: "
+Console.Write("Informe um numero: ");
+// Crie a variavel variavelQueSalvaUmNumero, 
+// leia o que o usuario digitar 
+// e transformar o valor lido de texto para interiro(int.Parse)
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine());
+
+// verificar se o valor lido de variavelQueSakvaUmNumero é par
+if (variavelQueSalvaUmNumero % 2 == 0 ) {
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
+}
+else {
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é ímpar");
+}
